21c5c28284c2494e1e42c79dbaceb7455b783f48
febbraiod <donedesign1@gmail.com>
2016-11-02 11:55:21 -0400

    adjust columns and blending content for clarity of subject, dry up blend html by using js and partials

---
+++ app/assets/javascripts/blend.js
@@ -0,0 +1,8 @@
+$(function(){
+  addBlendingContent();
+});
+
+
+function addBlendingContent(){
+  $('.blender').html("<h5><span id='redtype'>R</span><span id='greentype'>G</span><span id='bluetype'>B</span><br><span id='cyantype'>C</span><span id='yellowtype'>Y</span><span id='magentatype'>M</span><br><span id='blacktype'>K</span><span id='whitetype'>W</span><br><span id='lightgreytype'>LG</span><span id='darkgreytype'>DG</span></h5>");
+}
